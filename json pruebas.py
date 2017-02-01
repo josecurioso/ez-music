2d405894644f99becb44c5342d7d11899a3ec9fb
Jose Manuel Estrada-Nora Muñoz <jotajotanora@gmail.com>
2017-02-01 11:53:32 +0100

    Implemented downloading
    A big bug

--- json pruebas.py
+++ json pruebas.py
@@ -1,14 +1,40 @@
 import json
 import urllib
 
+class MyOpener(urllib.FancyURLopener):
+    version = "User-Agent=Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)"
+
+def is_obj( data ):
+    return type(data) == dict
+
+def download(link, filename):
+    #Creando la ruta para guardar
+    local_tmp_file = filename
+    print local_tmp_file
+
+    # Obteniendo archivo
+    my_urlopener = MyOpener()
+    print link
+    my_urlopener.addheader('Referer', link)
+    postparams = None
+    response = my_urlopener.open(link, postparams)
+
+    # Guardando archivo
+    local_file_handle = open(local_tmp_file, "w+")
+    local_file_handle.write(response.read())
+    local_file_handle.close()
+
+
 
 page = urllib.urlopen('http://www.youtubeinmp3.com/fetch/?format=json&video=https://www.youtube.com/watch?v=BOifp0ulmUc')
 
 data = page.read()
 
+decoded = json.loads(data)
 
-data_string = json.dumps(data)
-print 'ENCODED:', data_string
+print is_obj(decoded)
 
-decoded = json.loads(data_string)
-print 'DECODED:', decoded
+print decoded['title']
+print decoded['link']
+
+download(decoded['link'], decoded['title']+'.mp3')
